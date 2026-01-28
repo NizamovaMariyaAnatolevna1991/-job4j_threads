d87342fd022b40a31c3be6b185307dfd0011e0f2
masha <musha_skorpion@mail.ru>
2026-01-28 11:59:59 +0500

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        int index = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.print("\r load: " + process[index]);
+                index = (index + 1) % process.length;
+                Thread.sleep(500);
+            }
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.print("\r                     \r");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}

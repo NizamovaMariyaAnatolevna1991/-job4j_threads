efd7783acf245f9597d2b744d5071c9ef9251681
masha <musha_skorpion@mail.ru>
2026-04-09 19:03:57 +0500

    2. Обеспечить остановку потребителя. [#66825 #561549]

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -5,30 +5,37 @@ import ru.job4j.SimpleBlockingQueue;
 public class ParallelSearch {
     public static void main(String[] args) {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(3);
-        final Thread consumer = new Thread(
-                () -> {
-                    for (int index = 0; index < 3; index++) {
-                        try {
-                            System.out.println(queue.poll());
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                            Thread.currentThread().interrupt();
-                        }
-                    }
+        Thread consumer = new Thread(() -> {
+            for (int index = 0; index < 3; index++) {
+                try {
+                    System.out.println(queue.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
                 }
-        );
+            }
+        });
+
+        Thread producer = new Thread(() -> {
+            for (int index = 0; index != 3; index++) {
+                try {
+                    queue.offer(index);
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+
         consumer.start();
-        new Thread(
-                () -> {
-                    for (int index = 0; index != 3; index++) {
-                        try {
-                            queue.offer(index);
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            Thread.currentThread().interrupt();
-                        }
-                    }
-                }
-        ).start();
+        producer.start();
+
+        try {
+            producer.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        consumer.interrupt();
     }
 }
